7a7b6a85844a31830e785501062e8185d551fed8
fadilabdil <fadilabdillah0711@gmail.com>
2022-12-17 13:20:57 +0700

    upload file assignment

---
+++ src/Task.js
@@ -0,0 +1,5 @@
+const url = "YOURDEPLOYURL" // TODO: replace this
+const studentName = "Fadil Abdillah Suyudi" // TODO: replace this
+const studentId = "FE2737760" // TODO: replace this
+
+export { url, studentName, studentId }
